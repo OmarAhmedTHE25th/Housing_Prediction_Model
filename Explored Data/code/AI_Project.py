ed44b49a09e61353917d401936a4292d9a3efac9
Marwan <gowailymarwan@gmail.com>
2026-04-21 21:53:47 +0200

    Add Explored Data file

---
+++ Explored Data/code/AI_Project.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+
+# Load the dataset
+data_train = pd.read_csv('C:\\University\\Spring 2026\\Artificial Intelligence\\Project\\train.csv')
+data_test = pd.read_csv('C:\\University\\Spring 2026\\Artificial Intelligence\\Project\\test.csv')
+# Display the first few rows of the dataset
+print("Training Data:")
+print(data_train.head())
+print("\n")
+print("Test Data:")
+print(data_test.head())
+print("\n")
+# Check for missing values
+print("Missing values in training data:")
+print(data_train.isnull().sum())
+print("\n")
+print("Missing values in test data:")
+print(data_test.isnull().sum())
+print("\n")
+# Show missing values in their places
+print("Training data with missing values:")
+print(data_train[data_train.isnull().any(axis=1)])
+print("\n")
+print("Test data with missing values:")
+print(data_test[data_test.isnull().any(axis=1)])
+print("\n")
+# Check the non numerical columns
+print("Non-numerical columns in training data:")
+print(data_train.select_dtypes(include=['object', 'string']).columns )
+print("\n"+"Non-numerical columns in test data:")
+print(data_test.select_dtypes(include=['object', 'string']).columns )
+
